1e73b8caf10d9ebce3fbbb1fc7cd43c6b071785a
Delvina <vinadelvina2001@gmail.com>
2024-10-19 00:42:20 +0700

    Ada Update lagi Pa

--- resources/views/pokemon/edit.blade.php
+++ resources/views/pokemon/edit.blade.php
@@ -1,82 +1,105 @@
 @extends('layouts.template')
 
-@section('title', 'Update pokemon')
+@section('title', 'Update Pokemon')
 
 @section('body')
-
-<div class="mt-4 p-5 bg-black text-white rounded">
-    <h1>Update Existing Pokemon</h1>
-</div>
-
-<div class="row my-5">
-    <div class="col-12 px-5">
-        @if ($errors->any())
-        <div class="alert alert-danger mt-4">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error}}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
-
-        <form action="{{ route('pokemon.update', $pokemon) }}" method="POST" enctype="multipart/form-data">
-            @method('PUT')
-            @csrf
-
-        <div class="form-group">
-            <label for="name">name</label>
-            <input type="text" class="form-control" id="name"  placeholder="name" name="name" required value="{{ old('name' , $pokemon->name) }}">
-        </div>
-        <div class="form-group">
-            <label for="species">species</label>
-            <input type="text" class="form-control" id="species"  placeholder="species" name="species" required value="{{ old('species' , $pokemon->species) }}">
+    <div class="container">
+        <div class="mt-4 p-5 bg-black text-white rounded">
+            <h1>Update Existing Pokemon</h1>
         </div>
 
-        <div class="form-group">
-            <label for="primary_type">primary_type</label>
-            <input type="text" class="form-control" id="primary_type"  placeholder="primary_type" name="primary_type" value="{{ old('primary_type' , $pokemon->primary_type) }}">
+        <div class="row mt-2 mb-5">
+            <div class="col-12">
+                @if ($errors->any())
+                    <div class="alert alert-danger mt-4">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                <form action="{{ route('pokemon.update', $pokemon) }}" method="POST" enctype="multipart/form-data">
+                    @method('PUT')
+                    @csrf
+
+                    <div class="form-group mb-3">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" required
+                            placeholder="Enter Name" value="{{ old('name', $pokemon->name) }}">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="species">Species</label>
+                        <input type="text" class="form-control" id="species" name="species" required
+                            placeholder="Enter Species" value="{{ old('species', $pokemon->species) }}">
+                    </div>
+
+                    <div class="form-group mb-2">
+                        <label for="primary_type">Primary Type</label>
+                        <select class="form-control" id="primary_type" name="primary_type" required>
+                            @foreach ($types as $type)
+                                <option value="{{ $type }}">{{ $type }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="weight">Weight (kg)</label>
+                        <input type="number" class="form-control" id="weight" name="weight" step="0.01"
+                            placeholder="Enter Weight" value="{{ old('weight', $pokemon->weight) }}">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="height">Height (m)</label>
+                        <input type="number" class="form-control" id="height" name="height" step="0.01"
+                            placeholder="Enter Height" value="{{ old('height', $pokemon->height) }}">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="hp">HP</label>
+                        <input type="number" class="form-control" id="hp" name="hp" placeholder=" HP"
+                            value="{{ old('hp', $pokemon->hp) }}">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="attack">Attack</label>
+                        <input type="number" class="form-control" id="attack" name="attack"
+                            placeholder="Enter Attack Points" value="{{ old('attack', $pokemon->attack) }}">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="defense">Defense</label>
+                        <input type="number" class="form-control" id="defense" name="defense"
+                            placeholder="Enter Defense Points" value="{{ old('defense', $pokemon->defense) }}">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="is_legendary">Is Legendary</label>
+                        <select class="form-control" id="is_legendary" name="is_legendary" required>
+                            <option value="0"
+                                {{ old('is_legendary', $pokemon->is_legendary) == 0 ? 'selected' : '' }}>No
+                            </option>
+                            <option value="1"
+                                {{ old('is_legendary', $pokemon->is_legendary) == 1 ? 'selected' : '' }}>Yes
+                            </option>
+                        </select>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="photo">Photo</label>
+                        <input type="file" class="form-control" id="photo" name="photo">
+                        @if ($pokemon->photo)
+                            <img src="{{ $pokemon->photo_url }}" class="mt-3" style="max-width: 400px;"
+                                alt="Current Photo" />
+                        @endif
+                    </div>
+
+                    <button type="submit" class="btn btn-primary btn-block">Save</button>
+                </form>
+            </div>
         </div>
 
-        <div class="form-group">
-            <label for="weight">weight</label>
-            <input type="number" class="form-control" id="weight"  placeholder="weight" name="weight" value="{{ old('weight' , $pokemon->weight) }}">
-        </div>
-        <div class="form-group">
-            <label for="height">height</label>
-            <input type="number" class="form-control" id="height"  placeholder="height" name="height" value="{{ old('height' , $pokemon->height) }}">
-        </div>
-        <div class="form-group">
-            <label for="hp">hp</label>
-            <input type="number" class="form-control" id="hp"  placeholder="hp" name="hp" value="{{ old('hp' , $pokemon->hp) }}">
-        </div>
-        <div class="form-group">
-            <label for="attack">attack</label>
-            <input type="number" class="form-control" id="attack"  placeholder="attack" name="attack" value="{{ old('attack' , $pokemon->attack) }}">
-        </div>
-        <div class="form-group">
-            <label for="defense">defense</label>
-            <input type="number" class="form-control" id="defense"  placeholder="defense" name="defense" value="{{ old('defense' , $pokemon->defense) }}">
-        </div>
-        <div class="form-group">
-            <label for="is_legendary">is_legendary</label>
-            <input type="checkbox" class="form-control" id="is_legendary"  placeholder="is_legendary" name="is_legendary" value="{{ old('is_legendary' , $pokemon->is_legendary) }}">
-            <option value="0">No</option>
-            <option value="1">Yes</option>
-        </div>
-
-        <div class="form-group">
-            <label for="photo">photo</label>
-            <input type="file" class="form-control" id="photo"  name="photo">
-            @if ($pokemon->photo)
-                <img src= "{{ $pokemon->photo_url }}" class="mt-3" style="max-width: 400px; "/>
-
-            @endif
-        </div>
-
-        <button type="submit" class="btn btn-primary btn-block">Save</button>
-        </form>
     </div>
-</div>
-
 @endsection
--- resources/views/pokemon/show.blade.php
+++ resources/views/pokemon/show.blade.php
@@ -3,48 +3,50 @@
 @section('title', "pokemon: $pokemon->name")
 
 @section('body')
-@if ($pokemon->photo)
-    <img src="{{ $pokemon->photo_url }}" class="rounded img-thumbnail mx-auto d-block my-3"/>
-@endif
+<div class="container my-5">
+    @if ($pokemon->photo)
+        <img src="{{ $pokemon->photo_url }}" class="rounded img-thumbnail mx-auto d-block my-3"/>
+    @endif
 
-<table class="table table-bordered">
-    <tbody>
-        <tr>
-            <th scope="row">name</th>
-            <td>{{ $pokemon->name }}</td>
-        </tr>
-        <tr>
-            <th scope="row">species</th>
-            <td>{{ $pokemon->species }}</td>
-        </tr>
-        <tr>
-            <th scope="row">primary_type</th>
-            <td>{{ $pokemon->primary_type }}</td>
-        </tr>
-        <tr>
-            <th scope="row">weight</th>
-            <td>{{ $pokemon->weight }}</td>
-        </tr>
-        <tr>
-            <th scope="row">height</th>
-            <td>{{ $pokemon->height}}</td>
-        </tr>
-        <tr>
-            <th scope="row">hp</th>
-            <td>{{ $pokemon->hp }}</td>
-        </tr>
-        <tr>
-            <th scope="row">attack</th>
-            <td>{{ $pokemon->attack }}</td>
-        </tr>
-        <tr>
-            <th scope="row">defense</th>
-            <td>{{ $pokemon->defense }}</td>
-        </tr>
-        <tr>
-            <th scope="row">is_legendary</th>
-            <td>{{ $pokemon->is_legendary }}</td>
-        </tr>
-    </tbody>
-</table>
+    <table class="table table-bordered">
+        <tbody>
+            <tr>
+                <th scope="row">Name</th>
+                <td>{{ $pokemon->name }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Species</th>
+                <td>{{ $pokemon->species }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Primary Type</th>
+                <td>{{ $pokemon->primary_type }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Weight</th>
+                <td>{{ $pokemon->weight }} kg</td>
+            </tr>
+            <tr>
+                <th scope="row">Height</th>
+                <td>{{ $pokemon->height }} m</td>
+            </tr>
+            <tr>
+                <th scope="row">HP</th>
+                <td>{{ $pokemon->hp }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Attack</th>
+                <td>{{ $pokemon->attack }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Defense</th>
+                <td>{{ $pokemon->defense }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Is Legendary</th>
+                <td>{{ $pokemon->is_legendary ? 'Yes' : 'No' }}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 @endsection
