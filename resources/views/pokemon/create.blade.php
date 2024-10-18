60e97700585fc14d873197cf58f472c11aeb5a03
Delvina <vinadelvina2001@gmail.com>
2024-10-19 00:15:23 +0700

    Ada Update lagi Pa

--- resources/views/pokemon/create.blade.php
+++ resources/views/pokemon/create.blade.php
@@ -1,65 +1,82 @@
-@extends('layouts.app')
-section('content')
-<div class="container">
-    <h1>Create New Pokemon</h1>
-    <form action="{{ route('pokemon.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="form-group">
-            <label for="id">id</label>
-            <input type="text" class="form-control" id="id" name="id" required maxlength="255">
-        </div>
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" name="name" required maxlength="255">
-        </div>
-        <div class="form-group">
-            <label for="species">Species</label>
-            <input type="text" class="form-control" id="species" name="species" required maxlength="100">
-        </div>
-        <div class="form-group">
-            <label for="primary_type">Primary Type</label>
-            <select class="form-control" id="primary_type" name="primary_type" required>
-                @foreach($types as $type)
-                    <option value="{{ $type }}">{{ $type }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="weight">Weight</label>
-            <input type="number" step="0.01" class="form-control" id="weight" name="weight" min="0" max="999999.99">
-        </div>
-        <div class="form-group">
-            <label for="height">Height</label>
-            <input type="number" step="0.01" class="form-control" id="height" name="height" min="0" max="999999.99">
-        </div>
-        <div class="form-group">
-            <label for="hp">HP</label>
-            <input type="number" class="form-control" id="hp" name="hp" min="0" max="9999">
-        </div>
-        <div class="form-group">
-            <label for="attack">Attack</label>
-            <input type="number" class="form-control" id="attack" name="attack" min="0" max="9999">
-        </div>
-        <div class="form-group">
-            <label for="defense">Defense</label>
-            <input type="number" class="form-control" id="defense" name="defense" min="0" max="9999">
-        </div>
-        <div class="form-group">
-            <label for="is_legendary">Is Legendary</label>
-            <select class="form-control" id="is_legendary" name="is_legendary">
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-            </select>
-        </div>
+@extends('layouts.template')
 
-        <div class="form-group">
-            <label for="photo">photo</label>
-            <input type="file" class="form-control" id="photo"  name="photo">
-        </div>
+@section('body')
+    <div class="container my-5">
+        <h1>Create New Pokemon</h1>
+        <form action="{{ route('pokemon.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
 
-        <button type="submit" class="btn btn-primary btn-block">Save</button>
+            {{-- <div class="form-group mb-2">
+                <label for="id">ID</label>
+                <input type="text" class="form-control" id="id" name="id" required maxlength="255"
+                    placeholder="Enter Pokemon ID">
+            </div> --}}
+
+            <div class="form-group mb-2">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" name="name" required maxlength="255"
+                    placeholder="Enter Pokemon Name">
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="species">Species</label>
+                <input type="text" class="form-control" id="species" name="species" required maxlength="100"
+                    placeholder="Enter Pokemon Species">
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="primary_type">Primary Type</label>
+                <select class="form-control" id="primary_type" name="primary_type" required>
+                    @foreach ($types as $type)
+                        <option value="{{ $type }}">{{ $type }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="weight">Weight (kg)</label>
+                <input type="number" step="0.01" class="form-control" id="weight" name="weight" min="0"
+                    max="999999.99" placeholder="Enter Weight">
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="height">Height (m)</label>
+                <input type="number" step="0.01" class="form-control" id="height" name="height" min="0"
+                    max="999999.99" placeholder="Enter Height">
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="hp">HP</label>
+                <input type="number" class="form-control" id="hp" name="hp" min="0" max="9999"
+                    placeholder="Enter HP">
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="attack">Attack</label>
+                <input type="number" class="form-control" id="attack" name="attack" min="0" max="9999"
+                    placeholder="Enter Attack Points">
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="defense">Defense</label>
+                <input type="number" class="form-control" id="defense" name="defense" min="0" max="9999"
+                    placeholder="Enter Defense Points">
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="is_legendary">Is Legendary</label>
+                <select class="form-control" id="is_legendary" name="is_legendary" required>
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div class="form-group mb-2">
+                <label for="photo">Photo</label>
+                <input type="file" class="form-control" id="photo" name="photo" accept="image/*">
+            </div>
+
+            <button type="submit" class="btn btn-primary btn-block">Save</button>
         </form>
     </div>
-</div>
-
 @endsection
