a6f36c3946ee5168f787e34cc62a86acb527e69f
Delvina <vinadelvina2001@gmail.com>
2024-10-19 15:29:39 +0700

    typo

--- app/Models/pokemon.php
+++ app/Models/pokemon.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Storage;
 class Pokemon extends Model
 {
     use HasFactory;
-    protected $filable  = [
+    protected $fillable  = [
         'name',
         'species',
         'primary_type',
